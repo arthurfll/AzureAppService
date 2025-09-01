b5c737bacff5fc46acf9ae920305e7cd187f1916
João Arthur Gonçalves Fontenelle <arthurfontenelle2017@gmail.com>
2025-08-31 21:58:44 -0300

    add: setup api .NET

---
+++ Api/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/ping", () =>
+{
+    return "pong";
+})
+.WithName("Index");
+
+app.Run();
+
