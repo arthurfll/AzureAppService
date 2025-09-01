5ccc52eb4680fdc28155d932be285bed108ca10b
João Arthur Gonçalves Fontenelle <arthurfontenelle2017@gmail.com>
2025-09-01 09:46:45 -0300

    tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
 resource "azurerm_resource_group" "cloud_native_devops_platform" {
   name     = "cloud-native-devops-platform"
   location = "Brazil South"
