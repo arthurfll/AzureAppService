b47eb097cf236b0fc5566c91e4cd43b179e80e5e
João Arthur Gonçalves Fontenelle <arthurfontenelle2017@gmail.com>
2025-09-01 11:08:06 -0300

    tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,18 +1,38 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+resource "azurerm_container_group" "aci_group" {
+  name                = "live-prometheus-group"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  ip_address_type     = "public"
+  dns_name_label      = "liveprometheusplatform"
+
+  container {
+    name   = "api"
+    image  = "arthurfll/dotnet-api:1.0"
+    cpu    = "0.5"
+    memory = "1.0"
+    ports {
+      port = 5000
     }
   }
-}
 
-provider "azurerm" {
-  features {}
-}
+  container {
+    name   = "prometheus"
+    image  = "prom/prometheus:v2.47.2"
+    cpu    = "0.5"
+    memory = "1.0"
+    ports {
+      port = 9090
+    }
+  }
 
-
-resource "azurerm_resource_group" "cloud_native_devops_platform" {
-  name     = "cloud-native-devops-platform"
-  location = "Brazil South"
+  container {
+    name   = "grafana"
+    image  = "grafana/grafana:10.2.2"
+    cpu    = "0.5"
+    memory = "0.5"
+    ports {
+      port = 3000
+    }
+  }
 }
--- commands.md
+++ commands.md
@@ -3,3 +3,4 @@ docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
 
 docker rmi -f $(docker images -q)
+
